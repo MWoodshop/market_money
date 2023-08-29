fcb1449d6d1c5dcd64a589070e4c28f94f60fecb
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-29 18:41:27 -0400

    Refactor: fixes JSON in controller

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,21 +1,29 @@
 class Api::V0::MarketsController < ApplicationController
   def index
     markets = Market.all
-    render json: markets.map { |market| market.as_json.merge({ vendor_count: market.vendor_count }) }
+    render json: { data: markets.map do |market|
+      {
+        id: market.id.to_s,
+        type: 'market',
+        attributes: market.attributes.merge({ vendor_count: market.vendor_count })
+      }
+    end }
   rescue StandardError => e
-    render json: { error: e.message }, status: 500
+    render json: { errors: [{ detail: e.message }] }, status: 500
   end
 
   def show
     market = Market.find(params[:id])
-    render json: { data: {
-      id: market.id,
-      type: 'market',
-      attributes: market.attributes.merge({ vendor_count: market.vendor_count })
-    } }, status: 200
+    render json: {
+      data: {
+        id: market.id,
+        type: 'market',
+        attributes: market.attributes.merge({ vendor_count: market.vendor_count })
+      }
+    }, status: 200
   rescue ActiveRecord::RecordNotFound => e
     render json: { errors: [{ detail: e.message }] }, status: 404
   rescue StandardError => e
-    render json: { error: e.message }, status: 500
+    render json: { errors: [{ detail: e.message }] }, status: 500
   end
 end
