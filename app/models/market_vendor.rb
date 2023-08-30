b853228fef23a7e1e335ecfccf7d17ace87da2e5
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-30 13:57:29 -0400

    Feat: adds validation

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,7 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
+
+  validates :market_id,
+            uniqueness: { scope: :vendor_id, message: 'association between market and vendor already exists' }
 end
