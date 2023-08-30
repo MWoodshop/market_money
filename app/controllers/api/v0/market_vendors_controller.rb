8384c9517221ff6c06dc2fa4a9277bec5224b421
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-30 18:19:25 -0400

    Refactor: changes create method Feat: adds destroy method

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -12,8 +12,19 @@ class Api::V0::MarketVendorsController < ApplicationController
 
   # 8. Create a Market Vendor
   def create
-    market = Market.find(params[:market_id])
-    vendor = Vendor.find(params[:vendor_id])
+    begin
+      market = Market.find(params[:market_id])
+      vendor = Vendor.find(params[:vendor_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: [{ detail: e.message }] }, status: :not_found
+      return
+    end
+
+    # Check if the market-vendor relationship already exists
+    if MarketVendor.exists?(market_id: market.id, vendor_id: vendor.id)
+      render json: { errors: ['This vendor is already associated with this market'] }, status: :unprocessable_entity
+      return
+    end
 
     market_vendor = MarketVendor.new(market:, vendor:)
 
@@ -22,8 +33,25 @@ class Api::V0::MarketVendorsController < ApplicationController
     else
       render json: { errors: market_vendor.errors.full_messages }, status: :unprocessable_entity
     end
-  rescue ActiveRecord::RecordNotFound => e
-    render json: { errors: [{ detail: e.message }] }, status: 404
+  rescue StandardError => e
+    render json: { errors: [{ detail: e.message }] }, status: :internal_server_error
+  end
+
+  # 9. Delete a Market Vendor
+  def destroy
+    params_for_removal = params.require(:market_vendor).permit(:market_id, :vendor_id)
+    market_id = params_for_removal[:market_id]
+    vendor_id = params_for_removal[:vendor_id]
+
+    market_vendor = MarketVendor.find_by(market_id:, vendor_id:)
+
+    if market_vendor
+      market_vendor.destroy
+      head :no_content # This sends a 204 status
+    else
+      render json: { errors: [{ detail: "No MarketVendor with market_id=#{market_id} AND vendor_id=#{vendor_id} exists" }] },
+             status: 404
+    end
   rescue StandardError => e
     render json: { errors: [{ detail: e.message }] }, status: 500
   end
