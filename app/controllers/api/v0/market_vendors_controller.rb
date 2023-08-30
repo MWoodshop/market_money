b08045ddef7700e703c7a427d0cf9973377aea00
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-30 12:56:23 -0400

    Refactor: updates controller to use serializer

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,17 +1,9 @@
 class Api::V0::MarketVendorsController < ApplicationController
-  # Get All Vendors for a Market
+  # 3. Get All Vendors for a Market
   def index
     market = Market.find(params[:market_id])
     vendors = market.vendors
-    render json: {
-      data: vendors.map do |vendor|
-        {
-          id: vendor.id.to_s,
-          type: 'vendors',
-          attributes: vendor.attributes.except('id', 'created_at', 'updated_at')
-        }
-      end
-    }, status: 200
+    render json: MarketVendorSerializer.new(vendors).serializable_hash.to_json, status: :ok
   rescue ActiveRecord::RecordNotFound => e
     render json: { errors: [{ detail: e.message }] }, status: 404
   rescue StandardError => e
