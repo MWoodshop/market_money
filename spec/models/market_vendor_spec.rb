e293e9a737a51b0aa764adb1e4d536e29b0fe2e9
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-30 14:01:12 -0400

    Test: adds test for model

--- spec/models/market_vendor_spec.rb
+++ spec/models/market_vendor_spec.rb
@@ -3,4 +3,11 @@ require 'rails_helper'
 RSpec.describe MarketVendor, type: :model do
   it { should belong_to(:market) }
   it { should belong_to(:vendor) }
+
+  describe 'validations' do
+    subject { create(:market_vendor) }
+    it {
+      should validate_uniqueness_of(:market_id).scoped_to(:vendor_id).with_message('association between market and vendor already exists')
+    }
+  end
 end
