f684006309129154fccb8a73faf5140552030f5a
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-30 18:19:37 -0400

    Feat: updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      # Get all markets and get one market
       resources :markets, only: %i[index show] do
-        # Get all vendors for a specific market
         resources :vendors, controller: 'market_vendors', only: [:index]
       end
       resources :vendors, only: %i[index show create update destroy]
-      resources :market_vendors, only: [:create]
+
+      # Explicitly adding destroy route for market_vendors
+      delete 'market_vendors', to: 'market_vendors#destroy'
+
+      resources :market_vendors, only: %i[create]
     end
   end
 end
