e9bf5c2e4fcdd1a67fde27dd9526aa758242bd91
Mike Wood <127050722+MWoodshop@users.noreply.github.com>
2023-08-31 12:28:50 -0400

    Test: updates test to actually trigger the request

--- spec/requests/api/v0/cash_dispensers_request_spec.rb
+++ spec/requests/api/v0/cash_dispensers_request_spec.rb
@@ -29,9 +29,16 @@ RSpec.describe 'Cash Dispensers', type: :request do
 
     # Handling exceptions
     it 'returns a 500 status and an error message' do
-      allow_any_instance_of(Faraday::Connection).to receive(:get).and_return(
-        double(Faraday::Response, status: 500, body: 'Internal Server Error')
-      )
+      allow_any_instance_of(Faraday::Connection).to receive(:get).and_raise(StandardError.new('Internal Server Error'))
+
+      market = create(:market, lat: 35.07904, lon: -106.60068)
+      get api_v0_market_nearest_atms_path(market.id),
+          headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+
+      expect(response.status).to eq(500)
+      json_response = JSON.parse(response.body)
+
+      expect(json_response['errors'][0]['detail']).to eq('Internal Server Error')
     end
   end
 end
